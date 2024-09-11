a56b194d97e7fb029136cf1f7ab1043634b6a523
Matt Bannert <bunny@hey.com>
2024-09-11 10:09:46 +0200

    added github actions deployment action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://atom.tailus.io'
+  site: 'https://h4sci.github.io'
 });
