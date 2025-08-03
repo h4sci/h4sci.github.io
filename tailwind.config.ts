29c62b132f1e9414cdca0fb29e147cb38aa85e1d
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-08-03 19:27:16 +0200

    added dark mode logo, and the option to toggle between light & dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ module.exports = {
         './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
         "./node_modules/@tailus/themer-**/dist/**/*.{js,ts}"
     ],
+    // sets actual colors to light or dark (not just button)
+    darkMode: 'class',
     plugins: [
         themer({
             palette: {
