2496ea7ae565691c31fd90a7e37b3262c944a148
Minna Heim <91681064+minnaheim@users.noreply.github.com>
2025-08-04 18:24:16 +0200

    added navbar, based on ZRUG webpage (no subpages yet)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(), 
+    icon({
+      include: {
+        'simple-icons': ['*'], // Include all simple-icons
+        'bx': ['*'], // Include all boxicons (for bxl-linkedin)
+      },
+    })
+  ],
   site: 'https://h4sci.github.io'
 });
